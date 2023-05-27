9fd7d389608185238225d484c4ee8c348663236d
Sonu <RAJS64163@GMAIL.COM>
2023-05-27 15:27:01 +0530

    iopd mini

---
+++ src/assets/songs/songs.js
@@ -0,0 +1,15 @@
+import music1 from "./Calm-Down-Calm-Down.mp3";
+import music2 from "./Unholy.mp3";
+import music3 from "./Unstoppable.mp3";
+import music4 from "./Tsunami.mp3";
+import music5 from "./Senorita.mp3";
+
+const songs = {
+	music1,
+	music2,
+	music3,
+	music4,
+	music5,
+};
+
+export default songs;
